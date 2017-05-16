d8838e9e247ca8f308332a6eb5a080560aac75a4
Dmitry Samodov <samodov.dmitry@gmail.com>
2017-05-16 19:27:38 +0300

    removed second AccountCacheService registration

--- src/MarginTrading.Services/Modules/ServicesModule.cs
+++ src/MarginTrading.Services/Modules/ServicesModule.cs
@@ -28,10 +28,6 @@ namespace MarginTrading.Services.Modules
 				.As<IAccountAssetsCacheService>()
 				.SingleInstance();
 
-			builder.RegisterType<AccountsCacheService>()
-				.As<IAccountsCacheService>()
-				.SingleInstance();
-
 			builder.RegisterType<AccountGroupCacheService>()
 				.AsSelf()
 				.As<IAccountGroupCacheService>()
