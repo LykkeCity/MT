9e2576942439419c3ab52f689b7ac40b92aa944e
Nuno Araujo <nunobaraujo@hotmail.com>
2017-10-11 16:35:42 +0100

    Changed back MtClient variables from Protected to Private

--- src/MarginTrading.Client/MtClient.cs
+++ src/MarginTrading.Client/MtClient.cs
@@ -15,12 +15,12 @@ namespace MarginTrading.Client
 {
     public class MtClient
     {
-        protected string _token;
-        protected string _notificationId;
-        protected string _serverAddress;
-        protected IWampRealmProxy _realmProxy;
-        protected IRpcMtFrontend _service;
-        protected IWampChannel _channel;
+        private string _token;
+        private string _notificationId;
+        private string _serverAddress;
+        private IWampRealmProxy _realmProxy;
+        private IRpcMtFrontend _service;
+        private IWampChannel _channel;
 
         public void Connect(ClientEnv env)
         {
