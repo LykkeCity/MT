7c30e659a46972c05bec09e5f8b124e8938c13b3
Nuno Araujo <nunobaraujo@hotmail.com>
2017-10-23 19:18:28 +0100

    Solver logger bug

--- src/MarginTrading.Brokers/MarginTrading.AccountHistoryBroker/Repositories/SqlRepositories/AccountTransactionsReportsSqlRepository.cs
+++ src/MarginTrading.Brokers/MarginTrading.AccountHistoryBroker/Repositories/SqlRepositories/AccountTransactionsReportsSqlRepository.cs
@@ -17,52 +17,52 @@ namespace MarginTrading.AccountHistoryBroker.Repositories.SqlRepositories
         private const string CreateTableScript = "CREATE TABLE [{0}](" +
             "[Id] [nvarchar](64) NOT NULL, " +
             "[Date] [datetime] NOT NULL," +
-            "[ClientId] [nvarchar] (64) NOT NULL, "+
-            "[AccountId] [nvarchar] (64) NOT NULL, "+
-            "[PositionId] [text] NULL, "+
+            "[ClientId] [nvarchar] (64) NOT NULL, " +
+            "[AccountId] [nvarchar] (64) NOT NULL, " +
+            "[PositionId] [text] NULL, " +
             "[Amount] [numeric] (18, 6) NOT NULL, " +
-            "[Balance] [numeric] (18, 6) NOT NULL, "+
-            "[Type] [nvarchar] (50) NOT NULL, "+
-            "[Comment] [text] NOT NULL, "+
+            "[Balance] [numeric] (18, 6) NOT NULL, " +
+            "[Type] [nvarchar] (50) NOT NULL, " +
+            "[Comment] [text] NOT NULL, " +
             "[WithdrawTransferLimit] [numeric] (18, 6) NOT NULL, " +
             "CONSTRAINT[PK_{0}] PRIMARY KEY CLUSTERED ([Id] ASC)" +
             ");";
 
         private readonly IDbConnection _connection;
         private readonly ILog _log;
-        
+
         public AccountTransactionsReportsSqlRepository(MarginSettings settings, ILog log)
         {
+            _log = log;
 #if DEBUG
-            _connection = new SqlConnection(@"Server=.\SQLEXPRESS;Database=StockExchange;User Id=sa;Password = na123456;");
+            _connection = new SqlConnection(@"Server=.\SQLEXPRESS1;Database=WampTlsLogs;User Id=sa;Password = na123456;");
 #else
             _connection = new SqlConnection(settings.Db.ReportsConnString);
 #endif
             CreateTableIfDoesntExists();
         }
-        
+
         public async Task InsertOrReplaceAsync(IAccountTransactionsReport entity)
-        {    
+        {
             string query = $"insert into {TableName} " +
                 "(Id, Date, AccountId, ClientId, Amount, Balance, WithdrawTransferLimit, Comment, Type) " +
                 " values " +
                 "(@Id ,@Date, @AccountId, @ClientId, @Amount, @Balance, @WithdrawTransferLimit, @Comment, @Type)";
 
-           await _connection.ExecuteAsync(query, entity);
+            await _connection.ExecuteAsync(query, entity);
         }
 
         private void CreateTableIfDoesntExists()
-        {            
+        {
             try
             {
-                // Open connection
                 _connection.Open();
                 try
                 {
                     // Check if table exists
                     var res = _connection.ExecuteScalar($"select top 1 Id from {TableName}");
                 }
-                catch
+                catch (SqlException)
                 {
                     try
                     {
@@ -78,10 +78,7 @@ namespace MarginTrading.AccountHistoryBroker.Repositories.SqlRepositories
             {
                 _log.WriteErrorAsync("AccountTransactionsReportsSqlRepository", "CreateTableIfDoesntExists", null, ex);
                 throw;
-            }        
-            
-
-            
+            }
         }
     }
 }
