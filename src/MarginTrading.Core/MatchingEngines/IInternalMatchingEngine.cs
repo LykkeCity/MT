5e6384c6d39fc72fa7c0c5d3f505816ad349f773
Andrey Anokhin <yashmel@mail.ru>
2017-10-26 16:05:30 +0300

    LWDEV-3390: fixed rebase

--- src/MarginTrading.Core/MatchingEngines/IInternalMatchingEngine.cs
+++ src/MarginTrading.Core/MatchingEngines/IInternalMatchingEngine.cs
@@ -1,11 +1,9 @@
-﻿using System.Collections.Generic;
-
-namespace MarginTrading.Core.MatchingEngines
+﻿namespace MarginTrading.Core.MatchingEngines
 {
     public interface IInternalMatchingEngine : IMatchingEngineBase
     {
         void SetOrders(SetOrderModel model);
-        Dictionary<string, OrderBook> GetOrderBook(List<string> marketMakerIds);
+        OrderBook GetOrderBook(string instrument);
         bool PingLock();
     }
 }
