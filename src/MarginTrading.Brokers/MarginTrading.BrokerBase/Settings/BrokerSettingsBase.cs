8bcaa398e50ca14e775a20aab38798c268aebef8
Andrey Anokhin <yashmel@mail.ru>
2017-11-01 19:16:44 +0300

    LWDEV-3546: marked optional settings

--- src/MarginTrading.Brokers/MarginTrading.BrokerBase/Settings/BrokerSettingsBase.cs
+++ src/MarginTrading.Brokers/MarginTrading.BrokerBase/Settings/BrokerSettingsBase.cs
@@ -1,9 +1,13 @@
-﻿namespace MarginTrading.BrokerBase.Settings
+﻿using Lykke.SettingsReader.Attributes;
+
+namespace MarginTrading.BrokerBase.Settings
 {
     public class BrokerSettingsBase
     {
         public string MtRabbitMqConnString { get; set; }
+        [Optional]
         public bool IsLive { get; set; }
+        [Optional]
         public string Env { get; set; }
     }
 }
