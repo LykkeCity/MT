3ae9bc3ccc0482f451e158445bcc17a5a1105cf0
Nuno Araujo <nunobaraujo@hotmail.com>
2017-11-07 11:26:53 +0000

    Added SqlMigration Project

---
+++ src/MarginTrading.SqlMigration/Models/AccountTransactionsReportsEntity.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarginTrading.SqlMigration.Models
+{
+    internal class AccountTransactionsReportsEntity : TableEntity, IAccountTransactionsReports
+    {
+        public string Id
+        {
+            get => RowKey;
+            set => RowKey = value;
+        }
+
+        public string AccountId
+        {
+            get => PartitionKey;
+            set => PartitionKey = value;
+        }
+
+        public double Amount { get; set; }
+        public double Balance { get; set; }
+        public string ClientId { get; set; }
+        public string Comment { get; set; }
+        public DateTime Date { get; set; }
+        public string Type { get; set; }
+        public double WithdrawTransferLimit { get; set; }
+        public string PositionId { get; set; }
+
+        decimal IAccountTransactionsReports.Amount => Convert.ToDecimal(Amount);
+        decimal IAccountTransactionsReports.Balance => Convert.ToDecimal(Balance);
+        decimal IAccountTransactionsReports.WithdrawTransferLimit => Convert.ToDecimal(WithdrawTransferLimit);
+    }
+}
---
+++ src/MarginTrading.SqlMigration/Models/IAccountTransactionsReportsEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MarginTrading.SqlMigration.Models
+{
+    internal interface IAccountTransactionsReports
+    {
+        string AccountId { get; }
+        decimal Amount { get; }
+        decimal Balance { get; }
+        string ClientId { get; }
+        string Comment { get; }
+        DateTime Date { get; }
+        string Id { get; }
+        string PositionId { get; }
+        string Type { get; }
+        decimal WithdrawTransferLimit { get; }
+    }
+}
