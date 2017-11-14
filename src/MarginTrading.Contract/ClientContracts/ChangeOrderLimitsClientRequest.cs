32cdaa6f84f11f2238ee61f7be81103390212e51
Andrey Anokhin <yashmel@mail.ru>
2017-11-14 20:06:59 +0300

    LWDEV-3947: handle null stop loss and take profit in change limits request

--- src/MarginTrading.Contract/ClientContracts/ChangeOrderLimitsClientRequest.cs
+++ src/MarginTrading.Contract/ClientContracts/ChangeOrderLimitsClientRequest.cs
@@ -4,8 +4,8 @@
     {
         public string OrderId { get; set; }
         public string AccountId { get; set; }
-        public decimal TakeProfit { get; set; }
-        public decimal StopLoss { get; set; }
-        public decimal ExpectedOpenPrice { get; set; }
+        public decimal? TakeProfit { get; set; }
+        public decimal? StopLoss { get; set; }
+        public decimal? ExpectedOpenPrice { get; set; }
     }
 }
