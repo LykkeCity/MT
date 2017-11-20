13923276b45a3d95a9e0db3f1b32b2eb09516de6
Andrey Anokhin <yashmel@mail.ru>
2017-11-20 20:21:07 +0300

    LWDEV-4047: fixed wamp descriptions

--- src/MarginTrading.Frontend/Wamp/IWampTopic.cs
+++ src/MarginTrading.Frontend/Wamp/IWampTopic.cs
@@ -6,14 +6,19 @@ namespace MarginTrading.Frontend.Wamp
 {
     public interface IWampTopic
     {
-        [DocMe(Name = "prices.update", Description = "sends prices for all instruments")]
+        [DocMe(Name = "prices.update", Description = " [ Obsolete ] sends prices for all instruments")]
         BidAskPairRabbitMqContract AllPricesUpdate();
+        
         [DocMe(Name = "prices.update.{instrumentId}", Description = "sends prices for specific instrument")]
         BidAskPairRabbitMqContract InstumentPricesUpdate();
-        [DocMe(Name = "user.{notificationId}", Description = "sends user updates on position, account changes and dictionaries")]
+        
+        [DocMe(Name = "user.{notificationId}", Description = " [ Obsolete ] sends user updates on position, account changes and dictionaries")]
+        NotifyResponse<T> UserUpdates<T>();
+        
+        [DocMe(Name = "user.updates.{notificationId}", Description = "sends user updates on position, account changes and dictionaries")]
         NotifyResponse UserUpdates();
+        
         [DocMe(Name = "trades", Description = "sends trades info")]
         TradeClientContract Trades();
-
     }
 }
