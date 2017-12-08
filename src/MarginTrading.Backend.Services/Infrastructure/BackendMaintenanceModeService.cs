bd38b2c843fdb376c89e7ab99b27cc01515bc83f
Andrey Anokhin <yashmel@mail.ru>
2017-12-08 12:32:20 +0300

    LWDEV-4347: slack notifications when maintenance mode is changed

--- src/MarginTrading.Backend.Services/Infrastructure/BackendMaintenanceModeService.cs
+++ src/MarginTrading.Backend.Services/Infrastructure/BackendMaintenanceModeService.cs
@@ -1,7 +1,22 @@
-﻿namespace MarginTrading.Backend.Services.Infrastructure
+﻿using Lykke.Logs;
+using Lykke.SlackNotifications;
+using MarginTrading.Backend.Core.Settings;
+using MarginTrading.Common.Enums;
+
+namespace MarginTrading.Backend.Services.Infrastructure
 {
     public class BackendMaintenanceModeService : IMaintenanceModeService
     {
+        private readonly ISlackNotificationsSender _slackNotificationsSender;
+        private readonly MarginSettings _settings;
+
+        public BackendMaintenanceModeService(ISlackNotificationsSender slackNotificationsSender,
+            MarginSettings settings)
+        {
+            _slackNotificationsSender = slackNotificationsSender;
+            _settings = settings;
+        }
+        
         private static bool IsEnabled { get; set; }
 
         public bool CheckIsEnabled()
@@ -12,6 +27,9 @@
         public void SetMode(bool isEnabled)
         {
             IsEnabled = isEnabled;
+
+            _slackNotificationsSender.SendAsync(ChannelTypes.Monitor, $"Backend {_settings.Env}",
+                $"Maintenance mode is {(isEnabled ? "ENABLED" : "DISABLED")}");
         }
     }
 }
