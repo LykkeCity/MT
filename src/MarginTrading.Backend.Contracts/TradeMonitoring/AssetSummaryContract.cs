15ca826c195fe5d607e89ac704094ead2630ca07
Nuno Araujo <nunobaraujo@hotmail.com>
2018-03-20 12:35:49 +0000

    LWDEV-6393: Implementing TradeMonitoring

---
+++ src/MarginTrading.Backend.Contracts/TradeMonitoring/AssetSummaryContract.cs
@@ -0,0 +1,13 @@
+﻿using JetBrains.Annotations;
+
+namespace MarginTrading.Backend.Contracts.TradeMonitoring
+{
+    [PublicAPI]
+    public class AssetSummaryContract
+    {
+        public string AssetPairId { get; set; }
+        public decimal VolumeLong { get; set; }
+        public decimal VolumeShort { get; set; }
+        public decimal PnL { get; set; }
+    }
+}
