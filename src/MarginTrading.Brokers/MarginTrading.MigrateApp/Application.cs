359b96589f28eacb04725a49800743b08f0aab39
mihail.yartsev <mihail.yartsev@gmail.com>
2018-03-28 10:52:23 +0300

    LWDEV-6560 order history cleanup script

---
+++ src/MarginTrading.Brokers/MarginTrading.MigrateApp/Application.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using AzureStorage.Tables;
+using Common.Log;
+using Lykke.SettingsReader;
+using Lykke.SlackNotifications;
+using MarginTrading.AccountReportsBroker.Repositories.AzureRepositories.Entities;
+using MarginTrading.AzureRepositories;
+using MarginTrading.Backend.Core;
+using MarginTrading.BrokerBase;
+using MarginTrading.BrokerBase.Settings;
+using MarginTrading.Contract.RabbitMqMessageModels;
+using MoreLinq;
+
+namespace MarginTrading.MigrateApp
+{
+    internal class Application : BrokerApplicationBase<BidAskPairRabbitMqContract>
+    {
+        private readonly Settings _settings;
+        private readonly IReloadingManager<Settings> _reloadingManager;
+
+        public Application(ILog logger, Settings settings,
+            CurrentApplicationInfo applicationInfo,
+            ISlackNotificationsSender slackNotificationsSender,
+            IReloadingManager<Settings> reloadingManager)
+            : base(logger, slackNotificationsSender, applicationInfo)
+        {
+            _settings = settings;
+            _reloadingManager = reloadingManager;
+        }
+
+        protected override BrokerSettingsBase Settings => _settings;
+        protected override string ExchangeName => "Fake";
+
+        protected override Task HandleMessage(BidAskPairRabbitMqContract message)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override void Run()
+        {
+            WriteInfoToLogAndSlack("Starting MigrateApp");
+
+            try
+            {
+                Task.WaitAll(
+                    Task.Run(ProcessOrders)
+                );
+                WriteInfoToLogAndSlack("MigrateApp finished");
+            }
+            catch (Exception ex)
+            {
+                _logger.WriteErrorAsync(ApplicationInfo.ApplicationFullName, "Application.RunAsync", null, ex)
+                    .GetAwaiter()
+                    .GetResult();
+            }
+        }
+
+        private async Task ProcessOrders()
+        {
+            var repository = AzureTableStorage<MarginTradingOrderHistoryEntity>.Create(
+                _reloadingManager.Nested(s => s.Db.MarginTradingConnString), "MarginTradingOrdersHistory", _logger);
+            var tasks = (await repository.GetDataAsync())
+                .Where(a => a.OrderUpdateType == null && a.Status != "Closed")
+                .GroupBy(a => a.PartitionKey)
+                .SelectMany(g => g.Batch(500))
+                .Select(batch => repository.DeleteAsync(batch));
+            await Task.WhenAll(tasks);
+        }
+    }
+}
---
+++ src/MarginTrading.Brokers/MarginTrading.MigrateApp/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Autofac;
+using Common.Log;
+using Lykke.SettingsReader;
+using MarginTrading.AzureRepositories;
+using MarginTrading.AzureRepositories.Contract;
+using MarginTrading.Backend.Core;
+using MarginTrading.BrokerBase;
+using MarginTrading.BrokerBase.Settings;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MarginTrading.MigrateApp
+{
+    public class Startup : BrokerStartupBase<DefaultBrokerApplicationSettings<Settings>, Settings>
+    {
+        protected override string ApplicationName => "MarginTradingMigrateApp";
+
+        public Startup(IHostingEnvironment env) : base(env)
+        {
+        }
+
+        protected override void RegisterCustomServices(IServiceCollection services, ContainerBuilder builder,
+            IReloadingManager<Settings> settings, ILog log, bool isLive)
+        {
+            builder.RegisterType<Application>().As<IBrokerApplication>().SingleInstance();
+        }
+    }
+}
