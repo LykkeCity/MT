1668682af9e271bb406e11ee4290704cff102b6c
Andrey Anokhin <yashmel@mail.ru>
2018-04-28 22:37:19 +0300

    LWDEV-7030: save orders history to sql

--- src/MarginTrading.Brokers/MarginTrading.OrderHistoryBroker/Application.cs
+++ src/MarginTrading.Brokers/MarginTrading.OrderHistoryBroker/Application.cs
@@ -11,25 +11,29 @@ namespace MarginTrading.OrderHistoryBroker
 {
     public class Application : BrokerApplicationBase<OrderFullContract>
     {
-        private readonly IMarginTradingOrdersHistoryRepository _ordersHistoryRepository;
+        private readonly IMarginTradingOrdersHistoryRepository[] _ordersHistoryRepositories;
         private readonly Settings _settings;
 
-        public Application(IMarginTradingOrdersHistoryRepository ordersHistoryRepository, ILog logger,
+        public Application(IMarginTradingOrdersHistoryRepository[] ordersHistoryRepositories, ILog logger,
             Settings settings, CurrentApplicationInfo applicationInfo,
             ISlackNotificationsSender slackNotificationsSender) : base(logger, slackNotificationsSender,
             applicationInfo)
         {
-            _ordersHistoryRepository = ordersHistoryRepository;
+            _ordersHistoryRepositories = ordersHistoryRepositories;
             _settings = settings;
         }
 
         protected override BrokerSettingsBase Settings => _settings;
         protected override string ExchangeName => _settings.RabbitMqQueues.OrderHistory.ExchangeName;
 
-        protected override Task HandleMessage(OrderFullContract order)
+        protected override async Task HandleMessage(OrderFullContract order)
         {
             var orderHistory = order.ToOrderHistoryDomain();
-            return _ordersHistoryRepository.AddAsync(orderHistory);
+
+            foreach (var ordersHistoryRepository in _ordersHistoryRepositories)
+            {
+                await ordersHistoryRepository.AddAsync(orderHistory);
+            }
         }
     }
 }
