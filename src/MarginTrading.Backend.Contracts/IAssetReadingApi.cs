c365693e8a8fe7f6b5ee32db0b3ae3a927bed209
val martinchik <valmartsin@gmail.com>
2018-08-01 12:39:42 +0300

    LWDEV-8437 adding asset controller for data reader and contract for it

---
+++ src/MarginTrading.Backend.Contracts/IAssetReadingApi.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using MarginTrading.Backend.Contracts.AssetPairSettings;
+using MarginTrading.Backend.Contracts.AssetSettings;
+using MarginTrading.Backend.Contracts.Client;
+using Refit;
+
+namespace MarginTrading.Backend.Contracts
+{
+    [PublicAPI]
+    public interface IAssetReadingApi
+    {
+        /// <summary>
+        /// Get all pairs.
+        /// Cached on client for 3 minutes 
+        /// </summary>
+        [Get("/api/Asset/"), ClientCaching(Minutes = 3)]
+        Task<List<AssetContract>> List();
+
+        /// <summary>
+        /// Get pair by id
+        /// </summary>
+        [Get("/api/Asset/{assetId}")]
+        Task<AssetContract> Get(string assetPairId);
+    }
+}
