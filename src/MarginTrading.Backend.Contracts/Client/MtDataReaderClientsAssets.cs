01e512f9fce088f1457525026124f1e6e8047c54
val martinchik <valmartsin@gmail.com>
2018-08-02 10:55:32 +0300

    LWDEV-8437 adding contracts interface and implementation in order to distiguish between demo and real site

---
+++ src/MarginTrading.Backend.Contracts/Client/MtDataReaderClientsAssets.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MarginTrading.Backend.Contracts.Client
+{
+    internal class MtBackendClientsAssets : IMtBackendClientsAssets
+    {
+        public IMtBackendClient Demo { get; }
+        public IMtBackendClient Live { get; }
+
+        public IMtBackendClient Get(bool isLive)
+        {
+            return isLive ? Live : Demo;
+        }
+
+        public MtBackendClientsAssets(IMtBackendClient demo, IMtBackendClient live)
+        {
+            Demo = demo ?? throw new ArgumentNullException(nameof(demo));
+            Live = live ?? throw new ArgumentNullException(nameof(live));
+        }
+    }
+}
