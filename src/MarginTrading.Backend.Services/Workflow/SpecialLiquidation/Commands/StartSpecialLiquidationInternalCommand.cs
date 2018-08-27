68ff1d2d4ebb711049bbe79a749cc1cbcdc1832c
Alexey Andreev <alexey.andreev@lykke.com>
2018-08-27 13:05:38 +0300

    MTC-279: stub of SpecialLiquidationCommandsHandler

--- src/MarginTrading.Backend.Services/Workflow/SpecialLiquidation/Commands/StartSpecialLiquidationInternalCommand.cs
+++ src/MarginTrading.Backend.Services/Workflow/SpecialLiquidation/Commands/StartSpecialLiquidationInternalCommand.cs
@@ -1,7 +1,15 @@
+using System;
+using MessagePack;
+
 namespace MarginTrading.Backend.Services.Workflow.SpecialLiquidation.Commands
 {
+    [MessagePackObject]
     public class StartSpecialLiquidationInternalCommand
     {
+        [Key(0)]
+        public string OperationId { get; set; }
         
+        [Key(1)]
+        public DateTime CreationTime { get; set; }
     }
 }
--- src/MarginTrading.Backend.Services/Workflow/SpecialLiquidation/SpecialLiquidationCommandsHandler.cs
+++ src/MarginTrading.Backend.Services/Workflow/SpecialLiquidation/SpecialLiquidationCommandsHandler.cs
@@ -1,24 +1,53 @@
 using System.Threading.Tasks;
 using JetBrains.Annotations;
 using Lykke.Cqrs;
+using MarginTrading.Backend.Core;
+using MarginTrading.Backend.Core.Extensions;
+using MarginTrading.Backend.Core.Repositories;
 using MarginTrading.Backend.Services.Workflow.SpecialLiquidation.Commands;
+using MarginTrading.Common.Services;
 
 namespace MarginTrading.Backend.Services.Workflow.SpecialLiquidation
 {
     [UsedImplicitly]
     public class SpecialLiquidationCommandsHandler
     {
-        private readonly 
+        private readonly IDateService _dateService;
+        private readonly IOperationExecutionInfoRepository _operationExecutionInfoRepository;
+
+        private const string OperationName = "SpecialLiquidation";
         
-        public SpecialLiquidationCommandsHandler()
+        public SpecialLiquidationCommandsHandler(
+            IDateService dateService,
+            IOperationExecutionInfoRepository operationExecutionInfoRepository)
         {
-            
+            _dateService = dateService;
+            _operationExecutionInfoRepository = operationExecutionInfoRepository;
         }
 
         [UsedImplicitly]
         private async Task Handle(StartSpecialLiquidationInternalCommand command, IEventPublisher publisher)
         {
-            
+            //ensure idempotency
+            var executionInfo = await _operationExecutionInfoRepository.GetOrAddAsync(
+                operationName: OperationName,
+                operationId: command.OperationId,
+                factory: () => new OperationExecutionInfo<OperationData>(
+                    operationName: OperationName,
+                    id: command.OperationId,
+                    lastModified: _dateService.Now(),
+                    data: new OperationData
+                    {
+                        State = OperationState.Initiated,
+                    }
+                ));
+
+            if (executionInfo.Data.SwitchState(OperationState.Initiated, OperationState.Started))
+            {
+                
+
+                await _operationExecutionInfoRepository.Save(executionInfo);
+            }
         }
     }
 }
