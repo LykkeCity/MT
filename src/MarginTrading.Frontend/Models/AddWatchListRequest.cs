509486bb0b8051ea8159c6f457d110d7fd9df19d
Andrey Anokhin <yashmel@mail.ru>
2018-09-09 12:39:09 +0300

    LWDEV-9183: new response model for watch lists

---
+++ src/MarginTrading.Frontend/Models/AddWatchListRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace MarginTrading.Frontend.Models
+{
+    public class AddWatchListRequest
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public List<string> AssetIds { get; set; }
+    }
+}
