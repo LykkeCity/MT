f9dfb003065961774643fa841350719c82dc695f
Alexey Andreev <alexey.andreev@lykke.com>
2018-09-23 16:34:50 +0300

    MTC-345: random thread safety

--- src/MarginTrading.Backend.Services/Infrastructure/SimpleIdentityGenerator.cs
+++ src/MarginTrading.Backend.Services/Infrastructure/SimpleIdentityGenerator.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Common;
 using MarginTrading.Backend.Core.Repositories;
@@ -13,6 +14,7 @@ namespace MarginTrading.Backend.Services.Infrastructure
         private long _currentId;
         private readonly Random _random = new Random();
         private const string Pool = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        private readonly SemaphoreSlim _semaphore = new SemaphoreSlim(1, 1);
         
         public SimpleIdentityGenerator()
         {
@@ -27,8 +29,16 @@ namespace MarginTrading.Backend.Services.Infrastructure
 
         public string GenerateAlphanumericId()
         {
-            var chars = Enumerable.Range(0, 10).Select(x => Pool[_random.Next(0, Pool.Length)]);
-            return new string(chars.ToArray());
+            _semaphore.Wait();
+            try
+            {
+                var chars = Enumerable.Range(0, 10).Select(x => Pool[_random.Next(0, Pool.Length)]);
+                return new string(chars.ToArray());
+            }
+            finally
+            {
+                _semaphore.Release();
+            }
         }
 
         public string GenerateGuid()
