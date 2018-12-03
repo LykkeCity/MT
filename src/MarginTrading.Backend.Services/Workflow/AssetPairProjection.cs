090e743f3d2b6e5769346ea1b3cf683e76d337c6
Alexey Andreev <alexey.andreev@lykke.com>
2018-12-03 14:27:18 +0300

    MTC-492: handle asset pair delete events

--- src/MarginTrading.Backend.Services/Workflow/AssetPairProjection.cs
+++ src/MarginTrading.Backend.Services/Workflow/AssetPairProjection.cs
@@ -1,3 +1,5 @@
+using System.Threading.Tasks;
+using Common.Log;
 using JetBrains.Annotations;
 using MarginTrading.Backend.Core;
 using MarginTrading.Backend.Core.MatchingEngines;
@@ -14,33 +16,54 @@ namespace MarginTrading.Backend.Services.Workflow
     public class AssetPairProjection
     {
         private readonly IAssetPairsCache _assetPairsCache;
+        private readonly ILog _log;
 
         public AssetPairProjection(
-            IAssetPairsCache assetPairsCache)
+            IAssetPairsCache assetPairsCache,
+            ILog log)
         {
             _assetPairsCache = assetPairsCache;
+            _log = log;
         }
 
         [UsedImplicitly]
-        public void Handle(AssetPairChangedEvent @event)
+        public async Task Handle(AssetPairChangedEvent @event)
         {
-            //idempotency handling is not required, it's ok if an object is updated multiple times
-            
-            _assetPairsCache.AddOrUpdate(new AssetPair(
-                id: @event.AssetPair.Id,
-                name: @event.AssetPair.Name,
-                baseAssetId: @event.AssetPair.BaseAssetId,
-                quoteAssetId: @event.AssetPair.QuoteAssetId,
-                accuracy: @event.AssetPair.Accuracy,
-                legalEntity: @event.AssetPair.LegalEntity,
-                basePairId: @event.AssetPair.BasePairId,
-                matchingEngineMode: @event.AssetPair.MatchingEngineMode.ToType<MatchingEngineMode>(),
-                stpMultiplierMarkupBid: @event.AssetPair.StpMultiplierMarkupBid,
-                stpMultiplierMarkupAsk: @event.AssetPair.StpMultiplierMarkupAsk,
-                isSuspended: @event.AssetPair.IsSuspended,
-                isFrozen: @event.AssetPair.IsFrozen,
-                isDiscontinued: @event.AssetPair.IsDiscontinued
-            ));
+            //deduplication is not required, it's ok if an object is updated multiple times
+            if (@event.AssetPair?.Id == null)
+            {
+                await _log.WriteWarningAsync(nameof(AssetPairProjection), nameof(Handle),
+                    "AssetPairChangedEvent contained no asset pair id");
+                return;
+            }
+
+            if (IsDelete(@event))
+            {
+                _assetPairsCache.Remove(@event.AssetPair.Id);
+            }
+            else
+            {
+                _assetPairsCache.AddOrUpdate(new AssetPair(
+                    id: @event.AssetPair.Id,
+                    name: @event.AssetPair.Name,
+                    baseAssetId: @event.AssetPair.BaseAssetId,
+                    quoteAssetId: @event.AssetPair.QuoteAssetId,
+                    accuracy: @event.AssetPair.Accuracy,
+                    legalEntity: @event.AssetPair.LegalEntity,
+                    basePairId: @event.AssetPair.BasePairId,
+                    matchingEngineMode: @event.AssetPair.MatchingEngineMode.ToType<MatchingEngineMode>(),
+                    stpMultiplierMarkupBid: @event.AssetPair.StpMultiplierMarkupBid,
+                    stpMultiplierMarkupAsk: @event.AssetPair.StpMultiplierMarkupAsk,
+                    isSuspended: @event.AssetPair.IsSuspended,
+                    isFrozen: @event.AssetPair.IsFrozen,
+                    isDiscontinued: @event.AssetPair.IsDiscontinued
+                ));
+            }
+        }
+
+        private bool IsDelete(AssetPairChangedEvent @event)
+        {
+            return @event.AssetPair.BaseAssetId == null || @event.AssetPair.QuoteAssetId == null;
         }
     }
 }
