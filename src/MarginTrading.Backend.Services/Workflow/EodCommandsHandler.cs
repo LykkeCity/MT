9d799e4397fc383f64d3fc59728cfef64a23c356
Alexey Andreev <alexey.andreev@lykke.com>
2019-09-08 12:35:13 +0300

    LT-1625: Remove operationExecutionInfo from Trading Engine in order to unblock re-run for snapshot creation

--- src/MarginTrading.Backend.Services/Workflow/EodCommandsHandler.cs
+++ src/MarginTrading.Backend.Services/Workflow/EodCommandsHandler.cs
@@ -24,73 +24,43 @@ namespace MarginTrading.Backend.Services.Workflow
         private readonly ISnapshotService _snapshotService;
         private readonly IDateService _dateService;
         private readonly ILog _log;
-        private readonly IChaosKitty _chaosKitty;
-        private readonly IOperationExecutionInfoRepository _operationExecutionInfoRepository;
-        private const string OperationName = "EOD";
 
-        public EodCommandsHandler(ISnapshotService snapshotService, IDateService dateService, ILog log, 
-            IChaosKitty chaosKitty, IOperationExecutionInfoRepository operationExecutionInfoRepository)
+        public EodCommandsHandler(ISnapshotService snapshotService, IDateService dateService, ILog log)
         {
             _snapshotService = snapshotService;
             _dateService = dateService;
             _log = log;
-            _chaosKitty = chaosKitty;
-            _operationExecutionInfoRepository = operationExecutionInfoRepository;
         }
         
         [UsedImplicitly]
         private async Task Handle(CreateSnapshotCommand command, IEventPublisher publisher)
         {
-            var executionInfo = await _operationExecutionInfoRepository.GetOrAddAsync(
-                operationName: OperationName,
-                operationId: command.OperationId,
-                factory: () => new OperationExecutionInfo<EodOperationData>(
-                    operationName: OperationName,
-                    id: command.OperationId,
-                    lastModified: _dateService.Now(),
-                    data: new EodOperationData
-                    {
-                        State = OperationState.Initiated,
-                        TradingDay = command.TradingDay,
-                    }
-                ));
-
-            if (executionInfo.Data.SwitchState(OperationState.Initiated, OperationState.Finished))
+            //deduplication is inside _snapshotService.MakeTradingDataSnapshot
+            try
             {
-                try
+                await _snapshotService.MakeTradingDataSnapshot(command.TradingDay, command.OperationId);
+                
+                publisher.PublishEvent(new SnapshotCreatedEvent
                 {
-                    await _snapshotService.MakeTradingDataSnapshot(executionInfo.Data.TradingDay, executionInfo.Id);
-                    
-                    publisher.PublishEvent(new SnapshotCreatedEvent
-                    {
-                        OperationId = executionInfo.Id,
-                        CreationTime = _dateService.Now(),
-                    });
-                }
-                catch (ArgumentException argumentException)
+                    OperationId = command.OperationId,
+                    CreationTime = _dateService.Now(),
+                });
+            }
+            catch (ArgumentException argumentException)
+            {
+                _log.WriteWarning(nameof(EodCommandsHandler), nameof(CreateSnapshotCommand),
+                    argumentException.Message, argumentException);
+            }
+            catch (Exception exception)
+            {
+                _log.WriteError(nameof(EodCommandsHandler), nameof(CreateSnapshotCommand), exception);
+                
+                publisher.PublishEvent(new SnapshotCreationFailedEvent
                 {
-                    _log.WriteWarning(nameof(EodCommandsHandler), nameof(CreateSnapshotCommand),
-                        argumentException.Message, argumentException);
-                    
-                    return;
-                }
-                catch (Exception exception)
-                {
-                    _log.WriteError(nameof(EodCommandsHandler), nameof(CreateSnapshotCommand), exception);
-                    
-                    publisher.PublishEvent(new SnapshotCreationFailedEvent
-                    {
-                        OperationId = executionInfo.Id,
-                        CreationTime = _dateService.Now(),
-                        FailReason = exception.Message,
-                    });
-                    
-                    return; // state is not switched => it will work just fine on EOD re-run.
-                }
-
-                _chaosKitty.Meow(command.OperationId);
-
-                await _operationExecutionInfoRepository.Save(executionInfo);
+                    OperationId = command.OperationId,
+                    CreationTime = _dateService.Now(),
+                    FailReason = exception.Message,
+                });
             }
         }
     }
