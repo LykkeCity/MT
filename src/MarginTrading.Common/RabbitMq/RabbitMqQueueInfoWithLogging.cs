3743347bf0d9cf1820844b70018238c3fd635e30
Iliyan Iliev <iliyan.iliev@primeholding.com>
2020-10-19 18:10:32 +0300

    LT-2673 make LogEventPublishing optional

--- src/MarginTrading.Common/RabbitMq/RabbitMqQueueInfoWithLogging.cs
+++ src/MarginTrading.Common/RabbitMq/RabbitMqQueueInfoWithLogging.cs
@@ -1,10 +1,13 @@
 ﻿// Copyright (c) 2019 Lykke Corp.
 // See the LICENSE file in the project root for more information.
 
+using Lykke.SettingsReader.Attributes;
+
 namespace MarginTrading.Common.RabbitMq
 {
     public class RabbitMqQueueInfoWithLogging : RabbitMqQueueInfo
     {
+        [Optional]
         public bool LogEventPublishing { get; set; } = true;
     }
 }
