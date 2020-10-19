35e02751e1668553fc1aec30be03d2b04f24e098
Iliyan Iliev <iliyan.iliev@primeholding.com>
2020-10-19 16:51:09 +0300

    LT-2673 Add logging of all business critical events

---
+++ src/MarginTrading.Common/RabbitMq/RabbitMqQueueInfoWithLogging.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) 2019 Lykke Corp.
+// See the LICENSE file in the project root for more information.
+
+namespace MarginTrading.Common.RabbitMq
+{
+    public class RabbitMqQueueInfoWithLogging : RabbitMqQueueInfo
+    {
+        public bool LogEventPublishing { get; set; } = true;
+    }
+}
