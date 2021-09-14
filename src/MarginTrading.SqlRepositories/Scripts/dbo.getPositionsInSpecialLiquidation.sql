9c0860204834569a0c7c7f86a191075d42e63edc
Andrei Tarutin <tarutin_andrew@mail.ru>
2021-09-14 12:23:49 +0300

    fix(LT-3530): fixed sql script to be executable on SQL Server 2016 version

--- src/MarginTrading.SqlRepositories/Scripts/dbo.getPositionsInSpecialLiquidation.sql
+++ src/MarginTrading.SqlRepositories/Scripts/dbo.getPositionsInSpecialLiquidation.sql
@@ -1,6 +1,8 @@
 -- Copyright (c) 2019 BNP Paribas Arbitrage. All rights reserved.
 
-CREATE OR ALTER PROCEDURE [dbo].[getPositionsInSpecialLiquidation]
+CREATE
+OR ALTER PROCEDURE
+[dbo].[getPositionsInSpecialLiquidation]
     @positions as [dbo].[PositionListDataType] READONLY
     AS
 -- =====================================================================================
@@ -9,20 +11,32 @@ CREATE OR ALTER PROCEDURE [dbo].[getPositionsInSpecialLiquidation]
 -- =====================================================================================
 
 BEGIN
-    SELECT
-        positions.Id as PositionId
-    FROM [dbo].[MarginTradingExecutionInfo] e
+SELECT positions.Id as PositionId
+FROM [dbo].[MarginTradingExecutionInfo] e
     CROSS APPLY
-        (
-            SELECT
-                TRIM('"' FROM value) AS Id
-            FROM STRING_SPLIT(TRIM('[]' FROM JSON_QUERY(e.Data, '$.PositionIds')), ',')
-        ) positions
-    WHERE
-        e.OperationName = 'SpecialLiquidation' AND
-        JSON_VALUE(e.Data, '$.State') NOT IN ('OnTheWayToFail', 'Failed', 'Cancelled')
-    
-    INTERSECT
-    
-    SELECT Id FROM @positions
+    (
+    SELECT
+    REPLACE(value, '"', '') as Id
+    FROM STRING_SPLIT(
+        REPLACE(
+            REPLACE(
+                JSON_QUERY(e.Data, '$.PositionIds'), 
+                '[', 
+                ''), 
+            ']', 
+            ''), 
+        ',')
+    ) positions
+WHERE
+    e.OperationName = 'SpecialLiquidation'
+  AND
+    JSON_VALUE(e.Data
+    , '$.State') NOT IN ('OnTheWayToFail'
+    , 'Failed'
+    , 'Cancelled')
+
+INTERSECT
+
+SELECT Id
+FROM @positions
 END
