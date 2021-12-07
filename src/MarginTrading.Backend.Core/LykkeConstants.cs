c9c09c789dadc87578af8bc75785d7f44996a04b
atarutin <andrey.tarutin@lykke-business.ch>
2021-12-07 14:47:30 +0300

    fix(LT-3700): using SYSTEM as originator and author when special liquidation was started manually with technical controller endpoint

--- src/MarginTrading.Backend.Core/LykkeConstants.cs
+++ src/MarginTrading.Backend.Core/LykkeConstants.cs
@@ -26,5 +26,6 @@ namespace MarginTrading.Backend.Core
         public const string DescendingOrder = "DESC";
 
         public const string LiquidationByCaAdditionalInfo = "{\"CreatedBy\":\"EOD\"}";
+        public const string LiquidationBySystemAdditionalInfo = "{\"CreatedBy\":\"System\"}";
     }
 }
