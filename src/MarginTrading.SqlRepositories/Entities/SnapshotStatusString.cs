cc871c31345681a2bcb5cb8385743de694b8f072
atarutin <andrey.tarutin@lykke-business.ch>
2021-12-23 11:15:45 +0300

    feat(LT-3666): implementing IConverible by SnapshotStatusString

--- src/MarginTrading.SqlRepositories/Entities/SnapshotStatusString.cs
+++ src/MarginTrading.SqlRepositories/Entities/SnapshotStatusString.cs
@@ -11,7 +11,7 @@ namespace MarginTrading.SqlRepositories.Entities
     /// <summary>
     /// Snapshot status as a string for using as a part of DB entity 
     /// </summary>
-    public class SnapshotStatusString
+    public class SnapshotStatusString : IConvertible
     {
         private readonly string _value;
         
@@ -52,5 +52,91 @@ namespace MarginTrading.SqlRepositories.Entities
         {
             return _value;
         }
+
+        #region IConvertible implementation
+        
+        public TypeCode GetTypeCode() => TypeCode.Object;
+
+        public bool ToBoolean(IFormatProvider? provider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte ToByte(IFormatProvider? provider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public char ToChar(IFormatProvider? provider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTime ToDateTime(IFormatProvider? provider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal ToDecimal(IFormatProvider? provider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double ToDouble(IFormatProvider? provider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public short ToInt16(IFormatProvider? provider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ToInt32(IFormatProvider? provider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public long ToInt64(IFormatProvider? provider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public sbyte ToSByte(IFormatProvider? provider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public float ToSingle(IFormatProvider? provider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ToString(IFormatProvider? provider)
+        {
+            return this._value;
+        }
+
+        public object ToType(Type conversionType, IFormatProvider? provider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ushort ToUInt16(IFormatProvider? provider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public uint ToUInt32(IFormatProvider? provider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ulong ToUInt64(IFormatProvider? provider)
+        {
+            throw new NotImplementedException();
+        }
+        
+        #endregion
     }
 }
