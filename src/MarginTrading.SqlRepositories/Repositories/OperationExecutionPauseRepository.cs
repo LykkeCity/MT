97ae093d356e71c4339056ae081ed3cc65f8a1c1
atarutin <andrey.tarutin@lykke-business.ch>
2022-03-17 17:16:20 +0300

    feat(:T-3852): diagnostic logging

--- src/MarginTrading.SqlRepositories/Repositories/OperationExecutionPauseRepository.cs
+++ src/MarginTrading.SqlRepositories/Repositories/OperationExecutionPauseRepository.cs
@@ -153,8 +153,8 @@ where Oid = @Oid", new
 select Oid, OperationId, OperationName, Source, CancellationSource, CreatedAt, EffectiveSince, State, Initiator, CancelledAt, CancellationEffectiveSince, CancellationInitiator from [dbo].[MarginTradingExecutionPause]
 where OperationId = @OperationId AND OperationName = @OperationName", new {OperationId = operationId, OperationName = operationName});
 
-                    _log.WriteInfoAsync(nameof(OperationExecutionPauseRepository), nameof(AddAsync),
-                        $"Object: {entities.Single().ToJson()}");
+                    await _log.WriteInfoAsync(nameof(OperationExecutionPauseRepository), nameof(AddAsync),
+                        $"Object: {(entities == null || entities.Count() == 0 ? "empty": entities.ToJson())}");
 
                     var result = entities
                         .Select<dynamic, Pause>(o => Pause.Initialize(o));
