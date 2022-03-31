28f16c764181b782bfcf7ecf86af4cc75c3ba4e7
atarutin <andrey.tarutin@lykke-business.ch>
2022-03-31 15:20:16 +0300

    feat(LT-3852): Initiator.cs implemented IConvertible

--- src/MarginTrading.Backend.Contracts/Common/Initiator.cs
+++ src/MarginTrading.Backend.Contracts/Common/Initiator.cs
@@ -7,7 +7,7 @@ using Newtonsoft.Json;
 namespace MarginTrading.Backend.Contracts.Common
 {
     [JsonConverter(typeof(InitiatorConverter))]
-    public sealed class Initiator : IEquatable<Initiator>
+    public sealed class Initiator : IEquatable<Initiator>, IConvertible
     {
         private readonly string _value;
 
@@ -39,5 +39,91 @@ namespace MarginTrading.Backend.Contracts.Common
         {
             return (_value != null ? _value.GetHashCode() : 0);
         }
+
+        #region IConvertible implementation
+
+        public TypeCode GetTypeCode() => TypeCode.Object;
+
+        public bool ToBoolean(IFormatProvider provider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte ToByte(IFormatProvider provider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public char ToChar(IFormatProvider provider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTime ToDateTime(IFormatProvider provider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal ToDecimal(IFormatProvider provider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double ToDouble(IFormatProvider provider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public short ToInt16(IFormatProvider provider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ToInt32(IFormatProvider provider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public long ToInt64(IFormatProvider provider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public sbyte ToSByte(IFormatProvider provider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public float ToSingle(IFormatProvider provider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ToString(IFormatProvider provider)
+        {
+            return _value;
+        }
+
+        public object ToType(Type conversionType, IFormatProvider provider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ushort ToUInt16(IFormatProvider provider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public uint ToUInt32(IFormatProvider provider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ulong ToUInt64(IFormatProvider provider)
+        {
+            throw new NotImplementedException();
+        }
+        
+        #endregion
     }
 }
