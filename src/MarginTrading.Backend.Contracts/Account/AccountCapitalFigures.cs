a8e2f97a5da76d516fe9fb690558efafb529c74a
gponomarev-lykke <gennady.ponomarev@lykke-business.ch>
2022-04-15 11:15:01 +0500

    fix(LT-3935): avoid crash in donut

--- src/MarginTrading.Backend.Contracts/Account/AccountCapitalFigures.cs
+++ src/MarginTrading.Backend.Contracts/Account/AccountCapitalFigures.cs
@@ -27,6 +27,9 @@ namespace MarginTrading.Backend.Contracts.Account
         public decimal TodayOtherAmount { get; set; }
         public decimal TodayStartBalance { get; set; }
 
-        public static AccountCapitalFigures Empty = new AccountCapitalFigures();
+        public static AccountCapitalFigures Empty = new AccountCapitalFigures()
+        {
+            AdditionalInfo = "{}",
+        };
     }
 }
