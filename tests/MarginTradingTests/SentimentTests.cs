eb436210308013ddd477a09ddc685e04d365b6cf
atarutin <andrey.tarutin@lykke-business.ch>
2022-07-22 13:53:54 +0300

    feat(LT-4065): added more unit tests for Sentiment

--- tests/MarginTradingTests/SentimentTests.cs
+++ tests/MarginTradingTests/SentimentTests.cs
@@ -10,25 +10,38 @@ namespace MarginTradingTests
     public class SentimentTests
     {
         [Test]
-        public void Create_Sentiment_With_Negative_Counter_Results_In_Zero_Counter()
+        public void Create_Sentiment_With_Negative_Counter_Results_In_Zero_Share()
         {
             var sentiment = new Sentiment("test product", -1);
 
-            var (shortCounter, longCounter) = sentiment;
+            var (shortShare, longShare) = sentiment;
             
-            Assert.AreEqual(0, shortCounter);
-            Assert.AreEqual(0, longCounter);
+            Assert.AreEqual(0, shortShare);
+            Assert.AreEqual(0, longShare);
         }
         
         [Test]
-        public void Sentiment_Long_Counter_Is_The_Remainder_Based_On_Short_Counter()
+        public void Sentiment_Long_Share_Is_The_Remainder_Based_On_Short_Share()
         {
             var sentiment = new Sentiment("test product", 8, 1);
             
-            var (shortCounter, longCounter) = sentiment;
+            var (shortShare, longShare) = sentiment;
             
-            Assert.AreEqual(88.89, shortCounter);
-            Assert.AreEqual(100 - 88.89, longCounter);
+            Assert.AreEqual(88.89, shortShare);
+            Assert.AreEqual(100 - 88.89, longShare);
+        }
+        
+        [Test]
+        public void Sentiment_Remove_Counter_Less_Than_Zero_Results_In_Zero_Share()
+        {
+            var sentiment = new Sentiment("test product", 8, 1);
+            
+            sentiment = sentiment.RemoveLong(2);
+            
+            var (shortShare, longShare) = sentiment;
+            
+            Assert.AreEqual(100, shortShare);
+            Assert.AreEqual(0, longShare);
         }
     }
 }
