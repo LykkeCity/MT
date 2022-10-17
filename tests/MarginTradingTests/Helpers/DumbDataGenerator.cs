272915bcd0498dd4ca6b7e10039d62a806bc7f67
atarutin <andrey.tarutin@lykke-business.ch>
2022-10-17 16:54:38 +0300

    Revert "fix(LT-4202): honoring code review comments"
    This reverts commit ba7c90be5636b3bee0afd06dbc845d13400ae868.

--- tests/MarginTradingTests/Helpers/DumbDataGenerator.cs
+++ tests/MarginTradingTests/Helpers/DumbDataGenerator.cs
@@ -1,7 +1,6 @@
 // Copyright (c) 2019 Lykke Corp.
 // See the LICENSE file in the project root for more information.
 
-using System;
 using System.Collections.Generic;
 using MarginTrading.Backend.Core.Orders;
 using MarginTrading.Backend.Core.Trading;
@@ -10,15 +9,10 @@ namespace MarginTradingTests.Helpers
 {
     public static class DumbDataGenerator
     {
-        public static Position GeneratePosition(string id = null,
-            string assetPairId = null,
-            decimal? volume = null,
-            decimal? margin = null,
-            string accountId = null,
-            decimal? openPrice = null)
+        public static Position GeneratePosition(string assetPairId = null, decimal? volume = null, decimal? margin = null, string accountId  = null)
         {
             var result = new Position(
-                id ?? "1",
+                "1",
                 1,
                 assetPairId ?? string.Empty,
                 volume ?? default,
@@ -31,7 +25,7 @@ namespace MarginTradingTests.Helpers
                 string.Empty,
                 default,
                 default,
-                openPrice ?? default,
+                default,
                 default,
                 string.Empty,
                 default,
@@ -49,39 +43,34 @@ namespace MarginTradingTests.Helpers
 
             return result;
         }
-
-        public static Order GenerateOrder(string assetPairId = null,
-            decimal? volume = null,
-            string accountId = null,
-            List<string> positionsToBeClosed = null,
-            OrderStatus? status = null) =>
+        
+        public static Order GenerateOrder(string assetPairId = null, decimal? volume = null, string accountId = null) =>
             new Order(
-                "1",
-                default,
-                assetPairId ?? string.Empty,
-                volume ?? default,
-                default,
-                default,
-                null,
-                accountId ?? string.Empty,
-                string.Empty,
-                string.Empty,
-                default,
-                string.Empty,
-                default,
-                string.Empty,
-                string.Empty,
-                default,
-                default,
-                string.Empty,
-                string.Empty,
-                default,
-                default,
-                default,
-                string.Empty,
-                default,
-                status ?? OrderStatus.Placed,
-                string.Empty,
-                positionsToBeClosed);
+                "1", 
+                default, 
+                assetPairId ?? string.Empty, 
+                volume ?? default, 
+                default, 
+                default, 
+                null, 
+                accountId ?? string.Empty, 
+                string.Empty, 
+                string.Empty, 
+                default, 
+                string.Empty, 
+                default, 
+                string.Empty, 
+                string.Empty, 
+                default, 
+                default, 
+                string.Empty, 
+                string.Empty, 
+                default, 
+                default, 
+                default, 
+                string.Empty, 
+                default, 
+                default, 
+                string.Empty);
     }
 }
