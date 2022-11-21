35eca4d1d159026f70c0f40078ac3e169f290c0d
atarutin <andrey.tarutin@lykke-business.ch>
2022-11-21 18:45:56 +0300

    fix(LT-4280): change body part log format to distinguish

--- src/MarginTrading.Backend/Middleware/GlobalErrorHandlerMiddleware.cs
+++ src/MarginTrading.Backend/Middleware/GlobalErrorHandlerMiddleware.cs
@@ -61,16 +61,18 @@ namespace MarginTrading.Backend.Middleware
 
             if (asInfo)
             {
-                await _log.WriteInfoAsync(nameof(GlobalErrorHandlerMiddleware), bodyPart, ex.Message);
+                await _log.WriteInfoAsync(nameof(GlobalErrorHandlerMiddleware), $"Body: {bodyPart}", ex.Message);
                 return;
             }
             
             if (ex.InnerException != null)
             {
-                await _log.WriteErrorAsync(nameof(GlobalErrorHandlerMiddleware), bodyPart, ex.InnerException);
+                await _log.WriteErrorAsync(nameof(GlobalErrorHandlerMiddleware), 
+                    $"Body: {bodyPart}", 
+                    ex.InnerException);
             }
 
-            await _log.WriteErrorAsync(nameof(GlobalErrorHandlerMiddleware), bodyPart, ex);
+            await _log.WriteErrorAsync(nameof(GlobalErrorHandlerMiddleware), $"Body: {bodyPart}", ex);
         }
     }
 }
