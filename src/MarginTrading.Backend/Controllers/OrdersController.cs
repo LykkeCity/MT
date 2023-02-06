3de863f94affd07181584fb12812c94e76466ff5
atarutin <andrey.tarutin@lykke-business.ch>
2023-02-06 18:37:57 +0300

    fix(LT-4364): forcing problem details error

--- src/MarginTrading.Backend/Controllers/OrdersController.cs
+++ src/MarginTrading.Backend/Controllers/OrdersController.cs
@@ -214,7 +214,7 @@ namespace MarginTrading.Backend.Controllers
             }
             else
             {
-                throw new Exception($"Couldn't update related order for position {positionId}");
+                throw new OrderValidationException("Related order not found", OrderValidationError.OrderNotFound);
             }
         }
 
