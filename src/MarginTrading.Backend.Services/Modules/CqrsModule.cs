8562921d9f8977b55a2e974f6261452286586d36
atarutin <andrey.tarutin@lykke-business.ch>
2023-02-10 16:28:17 +0300

    fix(LT-4280): bump LykkeBiz.Snow.Cqrs -> 3.0.0
    removed other RabbitMq and messaging library dependencies

--- src/MarginTrading.Backend.Services/Modules/CqrsModule.cs
+++ src/MarginTrading.Backend.Services/Modules/CqrsModule.cs
@@ -14,6 +14,7 @@ using Lykke.Cqrs.Configuration.Routing;
 using Lykke.Cqrs.Configuration.Saga;
 using Lykke.Cqrs.Middleware.Logging;
 using Lykke.MarginTrading.OrderBookService.Contracts.Models;
+using Lykke.Messaging.RabbitMq.Retry;
 using Lykke.Messaging.Serialization;
 using Lykke.Snow.Common.Correlation.Cqrs;
 using Lykke.Snow.Common.Startup;
@@ -84,7 +85,7 @@ namespace MarginTrading.Backend.Services.Modules
                 new RabbitMqConventionEndpointResolver("RabbitMq", SerializationFormat.MessagePack,
                     environment: _settings.EnvironmentName);
 
-            var log = new LykkeLoggerAdapter<CqrsModule>(ctx.Resolve<ILogger<CqrsModule>>());
+            var loggerFactory = ctx.Resolve<ILoggerFactory>();
 
             var registrations = new List<IRegistration>
             {
@@ -93,8 +94,8 @@ namespace MarginTrading.Backend.Services.Modules
                 RegisterSpecialLiquidationSaga(),
                 RegisterLiquidationSaga(),
                 RegisterContext(),
-                Register.CommandInterceptors(new DefaultCommandLoggingInterceptor(log)),
-                Register.EventInterceptors(new DefaultEventLoggingInterceptor(log))
+                Register.CommandInterceptors(new DefaultCommandLoggingInterceptor(loggerFactory)),
+                Register.EventInterceptors(new DefaultEventLoggingInterceptor(loggerFactory))
             };
 
             var fakeGavel = RegisterGavelContextIfNeeded();
@@ -106,8 +107,16 @@ namespace MarginTrading.Backend.Services.Modules
             {
                 Uri = new Uri(_settings.ConnectionString, UriKind.Absolute)
             };
-            var engine = new RabbitMqCqrsEngine(log, ctx.Resolve<IDependencyResolver>(), new DefaultEndpointProvider(), 
-                rabbitMqSettings.Endpoint.ToString(), rabbitMqSettings.UserName, rabbitMqSettings.Password, true, registrations.ToArray());
+            var engine = new RabbitMqCqrsEngine(ctx.Resolve<ILoggerFactory>(),
+                ctx.Resolve<IDependencyResolver>(),
+                new DefaultEndpointProvider(),
+                rabbitMqSettings.Endpoint.ToString(),
+                rabbitMqSettings.UserName,
+                rabbitMqSettings.Password,
+                true,
+                TimeSpan.FromSeconds(5),
+                ctx.Resolve<IRetryPolicyProvider>(),
+                registrations.ToArray());
             engine.SetReadHeadersAction(correlationManager.FetchCorrelationIfExists);
             engine.SetWriteHeadersFunc(correlationManager.BuildCorrelationHeadersIfExists);
 
