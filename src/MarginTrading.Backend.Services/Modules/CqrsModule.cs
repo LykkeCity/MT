7d9c1cfd3ece1e66a5513e9d449cd302fa828c2b
atarutin <andrey.tarutin@lykke-business.ch>
2023-02-10 16:28:17 +0300

    fix(LT-4280): bump LykkeBiz.Snow.Cqrs -> 3.0.0
    removed other RabbitMq and messaging library dependencies

--- src/MarginTrading.Backend.Services/Modules/CqrsModule.cs
+++ src/MarginTrading.Backend.Services/Modules/CqrsModule.cs
@@ -14,6 +14,7 @@ using Lykke.Cqrs.Configuration.Routing;
 using Lykke.Cqrs.Configuration.Saga;
 using Lykke.Cqrs.Middleware.Logging;
 using Lykke.MarginTrading.OrderBookService.Contracts.Models;
+using Lykke.Messaging.RabbitMq.Retry;
 using Lykke.Messaging.Serialization;
 using Lykke.Snow.Common.Correlation.Cqrs;
 using Lykke.Snow.Cqrs;
@@ -106,13 +107,15 @@ namespace MarginTrading.Backend.Services.Modules
             {
                 Uri = new Uri(_settings.ConnectionString, UriKind.Absolute)
             };
-            var engine = new RabbitMqCqrsEngine(loggerFactory,
+            var engine = new RabbitMqCqrsEngine(ctx.Resolve<ILoggerFactory>(),
                 ctx.Resolve<IDependencyResolver>(),
                 new DefaultEndpointProvider(),
                 rabbitMqSettings.Endpoint.ToString(),
                 rabbitMqSettings.UserName,
                 rabbitMqSettings.Password,
                 true,
+                TimeSpan.FromSeconds(5),
+                ctx.Resolve<IRetryPolicyProvider>(),
                 registrations.ToArray());
             engine.SetReadHeadersAction(correlationManager.FetchCorrelationIfExists);
             engine.SetWriteHeadersFunc(correlationManager.BuildCorrelationHeadersIfExists);
