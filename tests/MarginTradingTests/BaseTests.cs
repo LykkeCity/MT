35f82e77fb85ad0898d952b10e05cf95be42c289
atarutin <andrey.tarutin@lykke-business.ch>
2023-03-07 13:37:15 +0300

    fix(LT-4280): fix unit tests

--- tests/MarginTradingTests/BaseTests.cs
+++ tests/MarginTradingTests/BaseTests.cs
@@ -123,7 +123,7 @@ namespace MarginTradingTests
             }
 
             builder.RegisterModule(new CacheModule());
-            builder.RegisterModule(new ServicesModule());
+            builder.RegisterModule(new ServicesModule(marginSettings));
             builder.RegisterModule(new ManagersModule());
             
             builder.RegisterType<EventChannel<AccountBalanceChangedEventArgs>>()
