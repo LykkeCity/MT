b1812aedf05008accd9583c2640c005a00b2bbbb
atarutin <andrey.tarutin@lykke-business.ch>
2023-06-02 17:08:13 +0300

    Revert "introduce possibility to control messages durability from configuration"
    This reverts commit 315cfb0be7b1d232668fefda223e9f1dc4e08a66.

--- src/MarginTrading.Common/RabbitMq/RabbitMqPublisherInfo.cs
+++ src/MarginTrading.Common/RabbitMq/RabbitMqPublisherInfo.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) 2019 Lykke Corp.
 // See the LICENSE file in the project root for more information.
 
-using Lykke.SettingsReader.Attributes;
-
 namespace MarginTrading.Common.RabbitMq
 {
     public class RabbitMqPublisherInfo
@@ -11,19 +9,11 @@ namespace MarginTrading.Common.RabbitMq
 
         public RabbitMqSettings ToRabbitMqSettings(string connectionString)
         {
-            var result = new RabbitMqSettings
+            return  new RabbitMqSettings
             {
                 ConnectionString = connectionString,
                 ExchangeName = ExchangeName,
             };
-            
-            if (IsDurable.HasValue)
-                result.IsDurable = IsDurable.Value;
-            
-            return result;
         }
-        
-        [Optional]
-        public bool? IsDurable { get; set; }
     }
 }
