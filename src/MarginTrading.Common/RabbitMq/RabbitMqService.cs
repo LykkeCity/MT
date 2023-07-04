db20ed505c95bb1d863373144e4e06b31c0516af
atarutin <andrey.tarutin@lykke-business.ch>
2023-07-04 15:14:59 +0300

    add synchronous publishing to get statistics

--- src/MarginTrading.Common/RabbitMq/RabbitMqService.cs
+++ src/MarginTrading.Common/RabbitMq/RabbitMqService.cs
@@ -118,13 +118,23 @@ namespace MarginTrading.Common.RabbitMq
                 // https://andrewlock.net/making-getoradd-on-concurrentdictionary-thread-safe-using-lazy/
                 return new Lazy<IStartStop>(() =>
                 {
-                    s.UseBufferType(PublisherBufferTypes.LockFree);
-                    var publisher = new RabbitMqPublisher<TMessage>(_loggerFactory, s);
-                    
-                    if (s.IsDurable && _publishingQueueRepository != null)
-                        publisher.SetQueueRepository(_publishingQueueRepository);
+                    var isBestPricePublisher = s.ExchangeName == "lykke.mt.pricefeed";
+
+                    RabbitMqPublisher<TMessage> publisher;
+                    if (isBestPricePublisher)
+                    {
+                        publisher = new RabbitMqPublisher<TMessage>(_loggerFactory, s).PublishSynchronously();
+                        _logger.WriteInfo(nameof(CreateProducer), null,
+                            $"Created synchronous RabbitMqPublisher for {s.ExchangeName}");
+                    }
                     else
-                        publisher.DisableInMemoryQueuePersistence();
+                    {
+                        publisher = new RabbitMqPublisher<TMessage>(_loggerFactory, s);
+                        if (s.IsDurable && _publishingQueueRepository != null)
+                            publisher.SetQueueRepository(_publishingQueueRepository);
+                        else
+                            publisher.DisableInMemoryQueuePersistence();
+                    }
 
                     var result = publisher
                         .SetSerializer(serializer)
