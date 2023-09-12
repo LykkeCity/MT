74b1fa4ea3f12102435a67be800c49e53e7b0f4d
atarutin <andrey.tarutin@lykke-business.ch>
2023-09-12 17:35:39 +0300

    Revert "Revert "Revert "Revert "add synchronous quotes publishing""""
    This reverts commit a15f5ed0fbb4343e5808cc2d69971bb90651fc1a.

--- src/MarginTrading.Common/RabbitMq/RabbitMqService.cs
+++ src/MarginTrading.Common/RabbitMq/RabbitMqService.cs
@@ -118,12 +118,23 @@ namespace MarginTrading.Common.RabbitMq
                 // https://andrewlock.net/making-getoradd-on-concurrentdictionary-thread-safe-using-lazy/
                 return new Lazy<IStartStop>(() =>
                 {
-                    var publisher = new RabbitMqPublisher<TMessage>(_loggerFactory, s);
+                    var isBestPricePublisher = s.ExchangeName == "lykke.mt.pricefeed";
 
-                    if (s.IsDurable && _publishingQueueRepository != null)
-                        publisher.SetQueueRepository(_publishingQueueRepository);
+                    RabbitMqPublisher<TMessage> publisher;
+                    if (isBestPricePublisher)
+                    {
+                        publisher = new RabbitMqPublisher<TMessage>(_loggerFactory, s).PublishSynchronously();
+                        _logger.WriteInfo(nameof(CreateProducer), null,
+                            $"Created synchronous RabbitMqPublisher for {s.ExchangeName}");
+                    }
                     else
-                        publisher.DisableInMemoryQueuePersistence();
+                    {
+                        publisher = new RabbitMqPublisher<TMessage>(_loggerFactory, s);
+                        if (s.IsDurable && _publishingQueueRepository != null)
+                            publisher.SetQueueRepository(_publishingQueueRepository);
+                        else
+                            publisher.DisableInMemoryQueuePersistence();
+                    }
 
                     var result = publisher
                         .SetSerializer(serializer)
