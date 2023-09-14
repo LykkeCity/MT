5068195696e15e93cf084f2a7800ba08b423d144
atarutin <andrey.tarutin@lykke-business.ch>
2023-09-14 12:14:10 +0300

    fix(LT-4983): add bulk insertions mode for sql formatter
    add bulk insertions mode for sql formatter when generating performance statistics report

--- src/MarginTrading.Backend.Services/PerformanceStatisticsSqlFormatter.cs
+++ src/MarginTrading.Backend.Services/PerformanceStatisticsSqlFormatter.cs
@@ -1,7 +1,6 @@
 // Copyright (c) 2019 Lykke Corp.
 // See the LICENSE file in the project root for more information.
 
-using System.Linq;
 using System.Text;
 using MarginTrading.Backend.Services.Extensions;
 
@@ -14,12 +13,8 @@ namespace MarginTrading.Backend.Services
             var sql = new StringBuilder();
             sql.AppendLine(Ddl);
 
-            return PerformanceTracker.Statistics.Aggregate(sql, (sb, s) =>
-            {
-                var line = s.Value.ToSqlStatement(s.Key);
-                sb.AppendLine(line);
-                return sb;
-            }).ToString();
+            var bulkInsertions = PerformanceTracker.Statistics.ToBulkSqlStatement();
+            return sql.AppendLine(bulkInsertions).ToString();
         }
 
         private static string Ddl =>
