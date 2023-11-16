925f3052e509edab726726a045732a1d4833aa49
atarutin <andrey.tarutin@lykke-business.ch>
2023-11-16 14:23:37 +0300

    Revert "fix(LT-5004): remove redundant log messages"
    This reverts commit a3be0827836f5bd26892d924a18e3d25cb97faaa.

--- src/MarginTrading.Backend.Services/AssetPairs/ScheduleSettingsCacheService.cs
+++ src/MarginTrading.Backend.Services/AssetPairs/ScheduleSettingsCacheService.cs
@@ -261,6 +261,8 @@ namespace MarginTrading.Backend.Services.AssetPairs
 
             if (assetPair == null)
             {
+                _log.WriteWarningAsync(nameof(ScheduleSettingsCacheService), nameof(GetInstrumentTradingStatus),
+                    $"AssetPair [{assetPairId}] does not exist in cache. Trading is disabled.").GetAwaiter().GetResult();
                 return InstrumentTradingStatus.Disabled(InstrumentTradingDisabledReason.InstrumentNotFound);
             }
 
@@ -268,6 +270,8 @@ namespace MarginTrading.Backend.Services.AssetPairs
 
             if (!_marketStates.TryGetValue(assetPair.MarketId, out var marketState))
             {
+                _log.WriteWarningAsync(nameof(ScheduleSettingsCacheService), nameof(GetInstrumentTradingStatus),
+                    $"Market status of market [{assetPair.MarketId}] for asset pair [{assetPairId}] does not exist in cache. Trading is disabled.").GetAwaiter().GetResult();
                 return InstrumentTradingStatus.Disabled(InstrumentTradingDisabledReason.MarketStateNotFound);
             }
 
